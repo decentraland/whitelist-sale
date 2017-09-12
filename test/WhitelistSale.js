dfa05164c75397818d4f9713083d1184a0fc55a5
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-09-12 15:20:17 +0200

    Add tests for addUser and setEthLimitPerDay

--- test/WhitelistSale.js
+++ test/WhitelistSale.js
@@ -3,12 +3,13 @@ const BigNumber = web3.BigNumber
 const WhitelistSale = artifacts.require('./WhitelistSale.sol')
 
 contract('WhitelistSale', function (accounts) {
-  const rate = new BigNumber(1000)
+  const sender = accounts[1]
+  const baseLimitPerDayAmount = new BigNumber(42)
 
   let sale
 
   beforeEach(async function () {
-    sale = await WhitelistSale.deployed()
+    sale = await WhitelistSale.new()
   })
 
   it('should activate the whitelist', async function () {
@@ -19,5 +20,33 @@ contract('WhitelistSale', function (accounts) {
 
     activated = await sale.activated.call()
     assert.equal(activated, true)
+  })
+
+  it('should set the ETH limit for a particular day', async function () {
+    let dayNum = 1
+    let limitPerDay
+
+    for (let day = 1; day < 7; day++) {
+      limitPerDay = await sale.limitPerDay.call(day)
+      assert.equal(limitPerDay, 0)
+    }
+
+    await sale.setEthLimitPerDay(dayNum, baseLimitPerDayAmount)
+
+    limitPerDay = await sale.limitPerDay.call(dayNum)
+    assert.equal(limitPerDay.toString(), baseLimitPerDayAmount.toString())
+  })
+
+  it('should add a user to the whitelist', async function () {
+    for (let day = 1; day < 7; day++) {
+      await sale.setEthLimitPerDay(day, baseLimitPerDayAmount.plus(day))
+    }
+
+    await sale.addUser(sender)
+
+    for (let day = 1; day < 7; day++) {
+      let allowOnDay = await sale.allowOnDay.call(day, sender)
+      assert.equal(allowOnDay.toString(), baseLimitPerDayAmount.plus(day))
+    }
   })
 })
